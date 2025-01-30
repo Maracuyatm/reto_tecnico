3be99ae5aa00c03382c1d58ca70fd5b900e9d0fe
Chris <neiltafurdev@gmail.com>
2025-01-30 15:55:19 -0500

    Creación del proyecto

---
+++ src/main/java/retoentelgy/animales/App.java
@@ -0,0 +1,13 @@
+package retoentelgy.animales;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
